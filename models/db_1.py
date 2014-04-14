0cb897778e3a37b599f0824e4902c4eea017e21e
Miranda, Martín <debianitram@gmail.com>
2014-04-13 23:04:02 -0300

    add views para las acciones administrar de contenedores y libros

---
+++ models/db_1.py
@@ -0,0 +1,45 @@
+### Requires.
+# Contenedor:
+Contenedor.nombre.requires = IS_NOT_EMPTY()
+
+# Libro
+Libro.titulo.requires = [IS_NOT_EMPTY(), IS_UPPER()]
+Libro.cantidad_total.requires = IS_NOT_EMPTY()
+Libro.ubicacion.requires = IS_IN_DB(db(Contenedor.es_contenedor == False),
+                                    Contenedor.id,
+                                    '%(nombre)s')
+# Libro.isbn.requires = IS_EMPTY_OR(IS_NOT_IN_DB(db, Libro))
+
+# Persona
+tipos = ['Alumno', 'Docente', 'No Docente']
+Persona.nombre.requires = IS_NOT_EMPTY()
+Persona.apellido.requires = IS_NOT_EMPTY()
+Persona.dni.requires = IS_NOT_IN_DB(db, Persona.dni)
+Persona.tipo.requires = IS_IN_SET(tipos)
+Persona.curso.requires = IS_NOT_EMPTY()
+
+# Movimientos
+estado_movimiento = {'1': 'Prestado', '2': 'Devuleto'}
+Movimientos.estado.requires = IS_IN_SET(estado_movimiento)
+### End Requires
+
+
+### Readable And Writables
+# Contenedor
+Contenedor.es_contenedor.readable = False
+Contenedor.es_contenedor.writable = False
+Contenedor.contenedor_superior.readable = False
+Contenedor.contenedor_superior.writable = False
+
+
+# Libro
+Libro.cantidad_prestados.readable = False
+Libro.cantidad_prestados.writable = False
+Libro.codsearch.readable = False
+### End Readable And Writables
+
+
+### Represent
+# Libro
+Libro.ubicacion.represent = lambda id, reg: str_libro_ubicacion(id)
+Libro.cantidad_disponible.represent = lambda v, r: SPAN(v, _class='badge')
